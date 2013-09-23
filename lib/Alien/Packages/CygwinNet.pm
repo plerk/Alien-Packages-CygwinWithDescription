feb79e50d85dea4ce5d31111418e148b93e190a5
Graham Ollis <perl@wdlabs.com>
2013-09-23 10:23:44 -0400

    Initial commit

---
+++ lib/Alien/Packages/CygwinNet.pm
@@ -0,0 +1,10 @@
+package Alien::Packages::CygwinNet;
+
+use strict;
+use warnings;
+use v5.10;
+
+# ABSTRACT: Get information rom Cygwin's packages via cygcheck and Cygwin::PackageDB
+# VERSION
+
+1;
